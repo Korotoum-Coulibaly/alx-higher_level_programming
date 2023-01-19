2e2acbe91ba25582bf2e377892962d39b6b088a5
Coulibaly Tiépégué Korotoum(Esther) <coulibalykorotoum0808@gmail.com>
2023-01-18 17:15:31 -0800

    ahh do hard things

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks if a singly linked list has a cycle in it
+ * @list: pointer to start node
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	current = list;
+	check = current->next;
+
+	while (current != NULL && check->next != NULL
+			&& check->next->next != NULL)
+	{
+		if (current == check)
+			return (1);
+		current = current->next;
+		check = check->next->next;
+	}
+	return (0);
+}
