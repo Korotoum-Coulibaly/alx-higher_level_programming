a97ddfe425c08438b591a58db70d385876497184
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-04 00:07:45 +0000

    task 4 and 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], (err, res, body) => {
+  if (err) console.log(err);
+  else {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) console.log(err);
+    });
+  }
+});
