257ce4e84ad10bd0851b4164705269a903b1c7a8
Esther Coulibaly <coulibalykorotoum0808@gmail.com>
2023-06-15 13:40:29 +0000

    advanced task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,7 +2,7 @@
 const myObject = {
   type: 'object',
   value: 12
-  };
+};
 console.log(myObject);
 
 myObject.incr = function () {
@@ -15,4 +15,3 @@ myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-  
